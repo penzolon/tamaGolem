cb6ecf91e75410a25acfa6142ea411b950e94e4e
LambertoCollo <alexbruschi05@gmail.com>
2025-05-12 16:48:07 +0200

    Utilizzo mappe e rimosso il JSON default

--- src/it/unibs/fp/tamaGolem/Equilibrio.java
+++ src/it/unibs/fp/tamaGolem/Equilibrio.java
@@ -1,41 +1,30 @@
 package it.unibs.fp.tamaGolem;
+
 import it.unibs.fp.myutil.inputOutput.AnsiColors;
 import it.unibs.fp.myutil.inputOutput.RandomDraws;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.io.File;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import com.google.gson.Gson;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Equilibrio {
-    private final int[][] equilibrio;
+    private final Map<String, Map<String, Integer>> equilibrioMap;
     private static String[] allElementi;
     private final List<String> elementi;
 
+    /**
+     * Costruttore che riceve il numero di elementi e il file JSON di definizione.
+     * Non utilizza alcun file di default.
+     */
     public Equilibrio(int numElementi, File file) {
         allElementi = JsonReader.parseFile(file);
-        equilibrio = new int[numElementi][numElementi];
-        elementi = new ArrayList<>();
-        estraiElementi(numElementi);
-        generaEquilibrio();
-    }
-
-    public Equilibrio(int numElementi) {
-        if(allElementi == null) {
-            Path path = Path.of(Costanti.ELEMENTI_DEFAULT);
-            Gson gson = new Gson();
-            try (FileReader reader = new FileReader(path.toFile())) {
-                allElementi = gson.fromJson(reader, String[].class);
-            } catch (IOException fileError) {
-                System.err.println("Errore nella lettura del file di default\n") ;
-                fileError.printStackTrace();
-            }
+        if (allElementi.length < numElementi) {
+            throw new IllegalArgumentException("Numero di elementi richiesto superiore a quelli definiti nel file.");
         }
-        equilibrio = new int[numElementi][numElementi];
+
+        equilibrioMap = new HashMap<>();
         elementi = new ArrayList<>();
         estraiElementi(numElementi);
         generaEquilibrio();
@@ -43,57 +32,74 @@ public class Equilibrio {
 
     private void estraiElementi(int numElementi) {
         List<Integer> interiEstratti = new ArrayList<>();
-        int interoEstratto;
         for (int i = 0; i < numElementi; i++) {
+            int interoEstratto;
             do {
                 interoEstratto = RandomDraws.drawInteger(0, allElementi.length - 1);
-            } while(interiEstratti.contains(interoEstratto));
+            } while (interiEstratti.contains(interoEstratto));
             interiEstratti.add(interoEstratto);
             elementi.add(allElementi[interoEstratto]);
         }
     }
 
     private void generaEquilibrio() {
-        int tentativi;
         boolean equilibrato = false;
+        int tentativiGlobali = 0;
 
-        while(!equilibrato) {
-            for (int[] riga: equilibrio) {
-                Arrays.fill(riga, 0);
+        while (!equilibrato) {
+            tentativiGlobali++;
+            if (tentativiGlobali > Costanti.MAX_TENTATIVI) {
+                throw new IllegalStateException("Impossibile generare un equilibrio stabile dopo " + Costanti.MAX_TENTATIVI + " tentativi globali.");
             }
 
-            for(int i = 0; i < equilibrio.length; i++) {
-                for(tentativi = 0; tentativi < Costanti.MAX_TENTATIVI && (sommaRiga(i) != 0 || rigaTuttaZero(i)); tentativi++) {
-                    for(int j = i + 1; j < equilibrio.length; j++) {
-                        equilibrio[i][j] = RandomDraws.drawInteger(-Costanti.MASSIMO_POTENZA, Costanti.MASSIMO_POTENZA);
-                        equilibrio[j][i] = -equilibrio[i][j];
+            equilibrioMap.clear();
+            // Inizializza mappe vuote per ogni elemento
+            for (String el : elementi) {
+                equilibrioMap.put(el, new HashMap<>());
+            }
+
+            // Genera interazioni mantenendo antisimmetria e prova a bilanciare riga per riga
+            boolean tutteRigheOk = true;
+            for (String elemI : elementi) {
+                Map<String, Integer> mappaI = equilibrioMap.get(elemI);
+                int tentativiRiga = 0;
+
+                do {
+                    mappaI.clear();
+                    for (String elemJ : elementi) {
+                        if (!elemI.equals(elemJ)) {
+                            int potenza = RandomDraws.drawInteger(-Costanti.MASSIMO_POTENZA, Costanti.MASSIMO_POTENZA);
+                            if (potenza == 0) potenza = 1;
+                            equilibrioMap.get(elemI).put(elemJ, potenza);
+                            equilibrioMap.get(elemJ).put(elemI, -potenza);
+                        }
                     }
-                }
-                if(tentativi == Costanti.MAX_TENTATIVI) {
-                    equilibrato = false;
+                    tentativiRiga++;
+                } while ((sommaInterazioni(elemI) != 0 || tutteNulle(elemI)) && tentativiRiga < Costanti.MAX_TENTATIVI);
+
+                if (tentativiRiga >= Costanti.MAX_TENTATIVI) {
+                    tutteRigheOk = false;
                     break;
-                } else {
-                    equilibrato = true;
                 }
             }
+
+            equilibrato = tutteRigheOk;
         }
     }
 
-    private int sommaRiga(int riga) {
-        int somma = 0;
-        for (int i = 0; i < equilibrio.length; i++) {
-            somma += equilibrio[riga][i];
-        }
-        return somma;
+    private int sommaInterazioni(String elemento) {
+        return equilibrioMap.get(elemento)
+                            .values()
+                            .stream()
+                            .mapToInt(Integer::intValue)
+                            .sum();
     }
 
-    private boolean rigaTuttaZero(int riga) {
-        for (int i = 0; i < equilibrio.length; i++) {
-            if (equilibrio[riga][i] != 0) {
-                return false;
-            }
-        }
-        return true;
+    private boolean tutteNulle(String elemento) {
+        return equilibrioMap.get(elemento)
+                            .values()
+                            .stream()
+                            .allMatch(val -> val == 0);
     }
 
     @Override
@@ -102,37 +108,36 @@ public class Equilibrio {
         String lineaSeparatrice = Costanti.LINEA_ORIZZONTALE.repeat(Costanti.LARGHEZZA_COLONNA);
         String lineaOrizzontale = Costanti.LINEA_ORIZZONTALE.repeat(Costanti.LARGHEZZA_COLONNA);
         final String lineaLegenda = Costanti.LINEA_VERTICALE.repeat(11);
-        
-        // Legenda decorata
+
         String[][] legendaItems = {
             {AnsiColors.YELLOW.toString(), Costanti.TESTO_ATTACCO_DEBOLE},
             {AnsiColors.RED.toString(), Costanti.TESTO_ATTACCO_FORTE},
             {AnsiColors.CYAN.toString(), Costanti.TESTO_DIFESA_DEBOLE},
             {AnsiColors.BLUE.toString(), Costanti.TESTO_DIFESA_FORTE}
         };
-        
+
         tabella.append(Costanti.ANGOLO_SUPERIORE_SINISTRO_LEGENDA).append(lineaLegenda)
-          .append(Costanti.TITOLO_LEGENDA)
-          .append(lineaLegenda)
-          .append(Costanti.ANGOLO_SUPERIORE_DESTRO_LEGENDA)
-          .append("\n");
+              .append(Costanti.TITOLO_LEGENDA)
+              .append(lineaLegenda)
+              .append(Costanti.ANGOLO_SUPERIORE_DESTRO_LEGENDA)
+              .append("\n");
 
         for (String[] item : legendaItems) {
             tabella.append(Costanti.BORDO_VERTICALE_LEGENDA).append(" ")
-              .append(item[0])
-              .append(centra((int)(Costanti.LARGHEZZA_COLONNA * 2.5), item[1]))
-              .append(AnsiColors.RESET)
-              .append(Costanti.BORDO_VERTICALE_LEGENDA)
-              .append("\n");
-    }
-    
-    tabella.append(Costanti.ANGOLO_INFERIORE_SINISTRO_LEGENDA)
-      .append(Costanti.LINEA_VERTICALE.repeat(31))
-      .append(Costanti.ANGOLO_INFERIORE_DESTRO_LEGENDA)
-      .append("\n").append("\n")
-      .append(Costanti.INTESTAZIONE_INTERPRETAZIONE).append("\n")
-      .append(Costanti.RIGA_ELEMENTI_ATTACCANTI).append("\n")
-      .append(Costanti.COLONNA_ELEMENTI_DIFESA).append("\n\n");
+                  .append(item[0])
+                  .append(centra((int)(Costanti.LARGHEZZA_COLONNA * 2.5), item[1]))
+                  .append(AnsiColors.RESET)
+                  .append(Costanti.BORDO_VERTICALE_LEGENDA)
+                  .append("\n");
+        }
+
+        tabella.append(Costanti.ANGOLO_INFERIORE_SINISTRO_LEGENDA)
+              .append(Costanti.LINEA_VERTICALE.repeat(31))
+              .append(Costanti.ANGOLO_INFERIORE_DESTRO_LEGENDA)
+              .append("\n\n")
+              .append(Costanti.INTESTAZIONE_INTERPRETAZIONE).append("\n")
+              .append(Costanti.RIGA_ELEMENTI_ATTACCANTI).append("\n")
+              .append(Costanti.COLONNA_ELEMENTI_DIFESA).append("\n\n");
 
         // Intestazione tabella
         tabella.append(Costanti.ANGOLO_SUPERIORE_SINISTRO_TABELLA).append(lineaSeparatrice);
@@ -141,47 +146,51 @@ public class Equilibrio {
         }
         tabella.append(Costanti.ANGOLO_SUPERIORE_DESTRO_TABELLA).append("\n");
 
-        // Prima riga con i nomi delle colonne
+        // Nomi colonne
         tabella.append(Costanti.BORDO_VERTICALE_TABELLA)
-          .append(" ".repeat(4))
-          .append(Costanti.FRECCIA_GIU)
-          .append(Costanti.DIF)
-          .append(" ".repeat(4));
+              .append(" ".repeat(4))
+              .append(Costanti.FRECCIA_GIU)
+              .append(Costanti.DIF)
+              .append(" ".repeat(4));
         for (String elemento : elementi) {
             tabella.append(Costanti.BORDO_VERTICALE_TABELLA)
-          .append(centra(Costanti.LARGHEZZA_COLONNA, elemento));
+                  .append(centra(Costanti.LARGHEZZA_COLONNA, elemento));
         }
         tabella.append(Costanti.BORDO_VERTICALE_TABELLA).append("\n");
 
-        // Linea orizzontale dopo i nomi delle colonne
+        // Riga separatrice
         tabella.append(Costanti.INCROCIO_SINISTRO).append(lineaSeparatrice);
         for (int k = 0; k < elementi.size(); k++) {
             tabella.append(Costanti.INCROCIO_CENTRALE).append(lineaOrizzontale);
         }
         tabella.append(Costanti.INCROCIO_DESTRO).append("\n");
 
-        // Contenuto tabella
+        // Corpo tabella
         for (int i = 0; i < elementi.size(); i++) {
+            String attaccante = elementi.get(i);
             tabella.append(Costanti.BORDO_VERTICALE_TABELLA)
-          .append(Costanti.ATT)
-          .append(Costanti.FRECCIA_DESTRA)
-          .append(centra(Costanti.LARGHEZZA_COLONNA - 4, elementi.get(i)));
-        
-        for (int j = 0; j < equilibrio[i].length; j++) {
-            tabella.append(Costanti.BORDO_VERTICALE_TABELLA)
-              .append(formattaNumero(equilibrio[i][j]))
-              .append(" ".repeat(6));
-        }
-        tabella.append(Costanti.BORDO_VERTICALE_TABELLA).append("\n");
-        
-        if (i < elementi.size() - 1) {
-            tabella.append(Costanti.INCROCIO_SINISTRO).append(lineaSeparatrice);
-            for (int k = 0; k < elementi.size(); k++) {
-                tabella.append(Costanti.INCROCIO_CENTRALE).append(lineaOrizzontale);
+                  .append(Costanti.ATT)
+                  .append(Costanti.FRECCIA_DESTRA)
+                  .append(centra(Costanti.LARGHEZZA_COLONNA - 4, attaccante));
+
+            for (int j = 0; j < elementi.size(); j++) {
+                String difensore = elementi.get(j);
+                int valore = attaccante.equals(difensore) ? 0 :
+                             equilibrioMap.get(attaccante).getOrDefault(difensore, 0);
+                tabella.append(Costanti.BORDO_VERTICALE_TABELLA)
+                      .append(formattaNumero(valore))
+                      .append(" ".repeat(6));
             }
-            tabella.append(Costanti.INCROCIO_DESTRO).append("\n");
+            tabella.append(Costanti.BORDO_VERTICALE_TABELLA).append("\n");
+
+            if (i < elementi.size() - 1) {
+                tabella.append(Costanti.INCROCIO_SINISTRO).append(lineaSeparatrice);
+                for (int k = 0; k < elementi.size(); k++) {
+                    tabella.append(Costanti.INCROCIO_CENTRALE).append(lineaOrizzontale);
+                }
+                tabella.append(Costanti.INCROCIO_DESTRO).append("\n");
+            }
         }
-    }
 
         // Chiusura tabella
         tabella.append(Costanti.ANGOLO_INFERIORE_SINISTRO_TABELLA).append(lineaSeparatrice);
@@ -189,12 +198,11 @@ public class Equilibrio {
             tabella.append(Costanti.INCROCIO_INFERIORE).append(lineaOrizzontale);
         }
         tabella.append(Costanti.ANGOLO_INFERIORE_DESTRO_TABELLA)
-      .append("\n")
-      .append("\n");
-    
-    return tabella.toString();
-}
-    
+              .append("\n\n");
+
+        return tabella.toString();
+    }
+
     private String formattaNumero(int numero) {
         String formato;
         String risultato;
--- src/it/unibs/fp/tamaGolem/JsonReader.java
+++ src/it/unibs/fp/tamaGolem/JsonReader.java
@@ -1,87 +1,98 @@
 package it.unibs.fp.tamaGolem;
-import java.io.File;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonReader {
     private final List<File> jsonFiles;
 
+    /**
+     * Scansiona la directory passata e costruisce la lista di file JSON validi.
+     * Non usa più un file di default: considera solo i file presenti nella directory.
+     */
     public JsonReader(String directoryPath) {
-        Path path = Path.of(directoryPath);
-        File directory = new File(path.toUri());
-        File defaultFile = new File(Costanti.ELEMENTI_DEFAULT);
-        File[] allFiles = directory.listFiles();
         jsonFiles = new ArrayList<>();
 
-        if (!(allFiles == null)) {
-            for (File file : allFiles) {
-                if (file.isFile() && file.getName().endsWith(".json")) {
-                    try (FileReader reader = new FileReader(file)) {
-                        JsonElement element = JsonParser.parseReader(reader);
+        // Verifica directory
+        File directory = Path.of(directoryPath).toFile();
+        File[] allFiles = directory.listFiles();
+        if (allFiles == null) {
+            System.err.println("[JsonReader] Directory non valida o non accessibile: " + directoryPath);
+            throw new IllegalStateException("Directory non valida: " + directoryPath);
+        }
 
-                        if (element.isJsonArray()) {
-                            JsonArray array = element.getAsJsonArray();
-
-                            if (array.size() >= 10) {
-                                boolean tuttoStringhe = true;
-
-                                for (JsonElement elemento : array) {
-                                    if (!elemento.isJsonPrimitive() || !elemento.getAsJsonPrimitive().isString()) {
-                                        tuttoStringhe = false;
-                                        break;
-                                    }
-                                }
-
-                                if (tuttoStringhe) {
-                                    jsonFiles.add(file);
+        for (File file : allFiles) {
+            if (file.isFile() && file.getName().endsWith(".json")) {
+                try (FileReader reader = new FileReader(file)) {
+                    JsonElement element = JsonParser.parseReader(reader);
+                    if (element.isJsonArray()) {
+                        JsonArray array = element.getAsJsonArray();
+                        if (array.size() >= Costanti.MIN_ELEMENTI) {
+                            boolean tuttoStringhe = true;
+                            for (JsonElement e : array) {
+                                if (!e.isJsonPrimitive() || !e.getAsJsonPrimitive().isString()) {
+                                    tuttoStringhe = false;
+                                    break;
                                 }
                             }
+                            if (tuttoStringhe) {
+                                jsonFiles.add(file);
+                            }
                         }
-                    } catch (IOException fileError) {}
+                    }
+                } catch (IOException ex) {
+                    System.err.println("[JsonReader] Errore parsing file: " + file.getName());
+                    ex.printStackTrace();
                 }
             }
         }
-        jsonFiles.add(defaultFile);
+
+        if (jsonFiles.isEmpty()) {
+            throw new IllegalStateException("Nessun file JSON valido trovato in " + directoryPath);
+        }
     }
 
     @Override
     public String toString() {
-        StringBuilder listaFile = new StringBuilder();
-
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < jsonFiles.size(); i++) {
-            listaFile.append(i+1).append(". ").append(jsonFiles.get(i).getName()).append("\n");
+            sb.append(i + 1)
+              .append(". ")
+              .append(jsonFiles.get(i).getName())
+              .append("\n");
         }
-
-        return listaFile.toString();
+        return sb.toString();
     }
 
     public File getFile(int index) {
-        return jsonFiles.get(index) ;
+        if (index < 0 || index >= jsonFiles.size()) {
+            throw new IndexOutOfBoundsException("Indice file JSON non valido: " + index);
+        }
+        return jsonFiles.get(index);
     }
 
     public int getSize() {
-        return jsonFiles.size() ;
+        return jsonFiles.size();
     }
 
+    /**
+     * Legge il file JSON e restituisce l'array di elementi (String[]).
+     */
     public static String[] parseFile(File file) {
-        String[] elementi ;
-
-        Gson gson = new Gson();
         try (FileReader reader = new FileReader(file)) {
-            elementi = gson.fromJson(reader, String[].class);
-        } catch (IOException fileError) {
-            System.err.println("Errore nella lettura del file") ;
-            elementi = new String[0];
+            return new Gson().fromJson(reader, String[].class);
+        } catch (IOException ex) {
+            System.err.println("[JsonReader] Errore nella lettura del file: " + file.getName());
+            ex.printStackTrace();
+            return new String[0];
         }
-
-        return elementi;
     }
 }
