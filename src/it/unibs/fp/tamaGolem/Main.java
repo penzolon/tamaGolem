0930025546cfefd8c0e1b1fc46f1f2209342f61d
penzolon <pezzonithomas23@gmail.com>
2025-05-13 11:20:44 +0200

    Spostato da Main ad InterfacciaUtente

--- src/it/unibs/fp/tamaGolem/Main.java
+++ src/it/unibs/fp/tamaGolem/Main.java
@@ -1,38 +1,8 @@
 package it.unibs.fp.tamaGolem;
-import it.unibs.fp.myutil.inputOutput.InputData ;
-import java.io.File;
 
 
 public class Main {
     public static void main(String[] args) {
-        int elementi = 0 ;
-        int numFile = 1 ;
-        File file = null ;
-        Equilibrio equilibrio ;
-        JsonReader jsonReader = new JsonReader(Costanti.ELEMENTI_PATH) ;
-
-        do {
-            if(elementi != 0) {
-                if(InputData.readYesOrNo("Cambiare il numero di elementi")) {
-                    elementi = InputData.readIntegerBetween("Inserisci il numero di elementi da considerare: ", Costanti.MIN_ELEMENTI, Costanti.MAX_ELEMENTI) ;
-                }
-            } else {
-                elementi = InputData.readIntegerBetween("Inserisci il numero di elementi da considerare: ", Costanti.MIN_ELEMENTI, Costanti.MAX_ELEMENTI) ;
-            }
-            if(file != null && jsonReader.getSize() > 1) {
-                if(InputData.readYesOrNo("Inserire un nuovo file di elementi")) {
-                    numFile = InputData.readIntegerBetween("Inserisci il numero del file da usare:\n\n"+jsonReader.toString(), 1, jsonReader.getSize()) ;
-                }
-            } else {
-                if (jsonReader.getSize() == 1) {
-                    System.out.println("Verrà utilizzato il file di default.\n") ;
-                } else {
-                    numFile = InputData.readIntegerBetween("Inserisci il numero del file da usare:\n\n"+jsonReader.toString(), 1, jsonReader.getSize()) ;
-                }
-            }
-            file = jsonReader.getFile(numFile - 1) ;
-            equilibrio = new Equilibrio(elementi, file) ;
-            System.out.println(equilibrio.toString());
-        } while(InputData.readYesOrNo("Continuare")) ;
+        InterfacciaUtente.sceltaElementi();
     }
 }
